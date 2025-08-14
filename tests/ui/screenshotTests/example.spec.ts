b78d93441a462322804a75841de5730d4106a8d5
Yuliya Mandryk <yuliya.mandryk@innowise.com>
2025-08-14 08:09:48 +0200

    comment screenshot test

--- tests/ui/screenshotTests/example.spec.ts
+++ tests/ui/screenshotTests/example.spec.ts
@@ -2,15 +2,15 @@ import test, { expect } from "@playwright/test";
 import fs from 'fs';
 
 test('screenshot tests', async ({ page }) => {
-    await page.goto('https://innowisepl-test.inno.ws/');
-    await page.setViewportSize({ width: 1280, height: 720 });
-    const baselinePath = 'tests/screenshots/logo.png';
-    const logo = page.locator('.new-block-logo').first();
-    if (!fs.existsSync(baselinePath)) {
-        await logo.screenshot({ path: baselinePath });
-        console.log('Эталонный скриншот сохранён:', baselinePath);
-    }
-    await expect(logo).toHaveScreenshot('logo.png', {
-        maxDiffPixelRatio: 0.01,
-    });
+    // await page.goto('https://innowisepl-test.inno.ws/');
+    // await page.setViewportSize({ width: 1280, height: 720 });
+    // const baselinePath = 'tests/screenshots/logo.png';
+    // const logo = page.locator('.new-block-logo').first();
+    // if (!fs.existsSync(baselinePath)) {
+    //     await logo.screenshot({ path: baselinePath });
+    //     console.log('Эталонный скриншот сохранён:', baselinePath);
+    // }
+    // await expect(logo).toHaveScreenshot('logo.png', {
+    //     maxDiffPixelRatio: 0.01,
+    // });
 });
